940de8e4221416a1db4496214a5a02fecf9a0edd
H Sharath Achar <sharathachar110@gmail.com>
2018-07-19 21:25:46 +0530

    added lines of code to return distinct characters of input string

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -8,7 +8,34 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
-            return false;
+            int count=0;
+            input = input.ToLower();
+            string inp = new string( input.Distinct().ToArray() );
+            for(int i=0; i<inp.Length; i++)
+                {
+                for(int j=0; j<alphabets.Length; j++)
+                    {
+                    if(inp[i] == alphabets[j])
+                        {
+                        count++;
+                        break;
+                        }
+                    }
+                if(count == 26)
+                    {
+                    break;
+                    }
+                }
+
+            if(count == 26)
+                {
+                return true;
+                }
+
+            else
+                {
+                return false;
+                }
         }
     }
 }
